bc51a26da1777d39df4f63daf11fc451907b9d58
mahaitao <769413715@qq.com>
2020-05-22 11:26:42 +0800

    reformat the code

--- offer/02替换空格/main.cpp
+++ offer/02替换空格/main.cpp
@@ -11,73 +11,73 @@
 using namespace std;
 
 class Solution1 {
-    public:
-		void replaceSpace(char* str, int length) {
-			// cout << str << endl;
-			int count = 0;
-			for (int i = 0; i < length; i++) {
-			    if (str[i] == ' ') {
-					count++;
-				}
-			}
-			char tmp[2*count+length+1];
-			count = 0;
-			for (int i = 0; i < length; i++) {
-			    if (str[i] == 32) {
-				    tmp[i+2*count] = '%';
-					tmp[i+2*count+1] = '2';
-					tmp[i+2*count+2] = 'B';
-					count++;
-				}
-				else {
-				    tmp[i+2*count] = str[i];
-				}
-			}
-			tmp[2*count+length] = '\0';
-			strcpy(str, tmp);
-		}
+public:
+
+    void replaceSpace(char* str, int length) {
+        // cout << str << endl;
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (str[i] == ' ') {
+                count++;
+            }
+        }
+        char tmp[2 * count + length + 1];
+        count = 0;
+        for (int i = 0; i < length; i++) {
+            if (str[i] == 32) {
+                tmp[i + 2 * count] = '%';
+                tmp[i + 2 * count + 1] = '2';
+                tmp[i + 2 * count + 2] = 'B';
+                count++;
+            } else {
+                tmp[i + 2 * count] = str[i];
+            }
+        }
+        tmp[2 * count + length] = '\0';
+        strcpy(str, tmp);
+    }
 };
 
 class Solution2 {
-    public:
-        void replaceSpace(char str[], int length) {
-            int count = 0;
-            for (int i = 0; i < length; i++) {
-                if (str[i] == ' ') {
-                    count++;
-                }
-            }
-            char *p1 = &str[length];
-            char *p2 = &str[length + 2*count];
-            while (p1 != p2) {
-                if (*p1 == ' ') {
-                    *p2-- = 'B';
-                    *p2-- = '2';
-                    *p2-- = '%';
-                }
-                else {
-                    *p2-- = *p1;
-                }
-                p1--;
+public:
+
+    void replaceSpace(char str[], int length) {
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (str[i] == ' ') {
+                count++;
             }
         }
+        char *p1 = &str[length];
+        char *p2 = &str[length + 2 * count];
+        while (p1 != p2) {
+            if (*p1 == ' ') {
+                *p2-- = 'B';
+                *p2-- = '2';
+                *p2-- = '%';
+            } else {
+                *p2-- = *p1;
+            }
+            p1--;
+        }
+    }
 };
 
 int main(int argc, char* argv[]) {
 
     /*
-	char s[] = "We     Are Happy";
-	cout << "origin str is: " << s << endl;
-	Solution1 sol1;
-	sol1.replaceSpace(s, strlen(s));
-	// char c = ' ';
-	// cout  << int(c) << endl;
-	cout << "after replace, the str is: " << s << endl;
-    */
+        char s[] = "We     Are Happy";
+        cout << "origin str is: " << s << endl;
+        Solution1 sol1;
+        sol1.replaceSpace(s, strlen(s));
+        // char c = ' ';
+        // cout  << int(c) << endl;
+        cout << "after replace, the str is: " << s << endl;
+     */
 
     char ss[] = "We are happy";
     cout << "origin str is: " << ss << endl;
-    
+
     Solution2 sol2;
     sol2.replaceSpace(ss, strlen(ss));
     cout << "after replace, the str is: " << ss << endl;
