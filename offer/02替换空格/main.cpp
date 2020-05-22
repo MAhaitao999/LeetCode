5c720f85c85db5c383da83a01835991c07276ea1
mahaitao <769413715@qq.com>
2020-05-22 11:22:02 +0800

    添加字符替换的第二种方法

--- offer/02替换空格/main.cpp
+++ offer/02替换空格/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 class Solution1 {
     public:
-		void replaceSpace(char str[], int length) {
+		void replaceSpace(char* str, int length) {
 			// cout << str << endl;
 			int count = 0;
 			for (int i = 0; i < length; i++) {
@@ -38,8 +38,34 @@ class Solution1 {
 		}
 };
 
+class Solution2 {
+    public:
+        void replaceSpace(char str[], int length) {
+            int count = 0;
+            for (int i = 0; i < length; i++) {
+                if (str[i] == ' ') {
+                    count++;
+                }
+            }
+            char *p1 = &str[length];
+            char *p2 = &str[length + 2*count];
+            while (p1 != p2) {
+                if (*p1 == ' ') {
+                    *p2-- = 'B';
+                    *p2-- = '2';
+                    *p2-- = '%';
+                }
+                else {
+                    *p2-- = *p1;
+                }
+                p1--;
+            }
+        }
+};
+
 int main(int argc, char* argv[]) {
 
+    /*
 	char s[] = "We     Are Happy";
 	cout << "origin str is: " << s << endl;
 	Solution1 sol1;
@@ -47,5 +73,13 @@ int main(int argc, char* argv[]) {
 	// char c = ' ';
 	// cout  << int(c) << endl;
 	cout << "after replace, the str is: " << s << endl;
+    */
+
+    char ss[] = "We are happy";
+    cout << "origin str is: " << ss << endl;
+    
+    Solution2 sol2;
+    sol2.replaceSpace(ss, strlen(ss));
+    cout << "after replace, the str is: " << ss << endl;
     return 0;
 }
